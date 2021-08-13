697b11551c1c06e118d08229fddadb96bf992321
Murugan K <murugank0204@gmail.com>
2021-08-13 20:23:48 +0530

    Update valid triangles.cpp

--- Day_063/valid triangles.cpp
+++ Day_063/valid triangles.cpp
@@ -1,18 +1,15 @@
 #include <bits/stdc++.h>
-using namespace std;
 
+using namespace std;
 #define fast() ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL)
-#define debug(x) cout << #x << "=" << x << endl
-#define debug2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define ll long long 
-#define ul unsigned long long int
 #define endl "\n"
 #define mod 1000000007
 //----------Working---Platform-------//
 void solve(){
     int a, b, c;
     cin >> a >> b >> c ;
-    if(a+b+c==180) cout << "YES"<<endl;
+    int sum = a+b+c;
+    if(sum==180) cout << "YES"<<endl;
     else cout << "NO" << endl;
 }
 int main(){
