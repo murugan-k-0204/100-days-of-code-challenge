c0d09cefec3275967d0c20ff17a3064079532591
Murugan K <83358275+murugan-k-0204@users.noreply.github.com>
2021-09-14 20:49:31 +0530

    Create Contains Duplicate II.cpp

---
+++ Day_095/Contains Duplicate II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if( i-k > 0) 
+                s.erase(nums[i-k-1]);
+            if(s.find(nums[i]) != s.end()) 
+                return true;
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};
